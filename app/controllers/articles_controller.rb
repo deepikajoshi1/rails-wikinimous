4438483bbcb0d5bfee626d8edaed21e114541421
deepika joshi <upreti.deepika@gmail.com>
2023-03-07 15:47:13 +0800

    added show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,30 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
+  def show
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to restaurants_path
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
